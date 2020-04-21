57f2bbdbc9acf7a1f2a4d40403bd499292988e86
Syed Danial Munsif <danial.munsif@quantis.it>
2020-04-21 15:54:08 +0500

    add internationalization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dynamic-translations';
+  public changeLanguage(code) {
+    localStorage.setItem('locale', code.target.value);
+    window.location.reload();
+  }
 }
